8df061a536b507a8d92846a8aeb49744f597d53d
Arjun Suresh <sarjun@zeomega.com>
2020-01-09 08:35:43 +0530

    ngrxstore sample implemenataion

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,23 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { StoreModule } from '@ngrx/store'
 
 import { AppComponent } from './app.component';
+import { reducer } from './reducers/tutorial.reducers';
+import { ReadComponent } from './read/read.component';
+import { CreateComponent } from './create/create.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ReadComponent,
+    CreateComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    StoreModule.forRoot({
+      tutorial: reducer
+    })
   ],
   providers: [],
   bootstrap: [AppComponent]
